818cb4ef2c36726a3c9d8b16d6f44ea015864d29
Sengeer <sengeer@mail.ru>
2023-10-22 14:52:16 +0300

    refactor: поправить footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,28 +6,28 @@ function Footer() {
     <footer
       className='footer' >
       <h2
-        className='footer__info footer__info_content_project'>
+        className='footer__info footer__info_content_project' >
           Учебный проект Яндекс.Практикум х BeatFilm.
       </h2 >
       <div
-        className='footer__links'>
+        className='footer__links' >
         <a
           href='https://practicum.yandex.ru/'
           rel="noreferrer"
           target='_blank'
-          className='footer__link'>
+          className='footer__link' >
             Яндекс.Практикум
         </a>
         <a
           href='https://github.com/Sengeer'
           rel="noreferrer"
           target='_blank'
-          className='footer__link'>
+          className='footer__link' >
             Github
         </a>
       </div>
       <p
-        className='footer__info footer__info_content_copyright'>
+        className='footer__info footer__info_content_copyright' >
           &#169;2023
       </p>
     </footer>
